f1aa823b0f957938a2c5a92b5a8db894a62d50b6
Bayu-aditiya-saputra <bayurikasolihat@gmail.com>
2022-01-05 21:57:28 +0700

    tugas 2 js
    tugas untuk niomic

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var usia = 22;
+var nama = "priyanto";
+var pria = true;
+var alamat;
+var hobi = ["footbal", "game", "Menonton Film"];
+
+console.log(usia);
+console.log(nama);
+console.log(pria);
+console.log(alamat);
+console.log(hobi);
